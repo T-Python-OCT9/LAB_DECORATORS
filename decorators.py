e67c27ced1de991e2ac97abfc4e5c1c521fc6521
wijdan alHarbi <wijdane.ali@gmail.com>
2022-10-18 23:17:23 +0300

    DECORATORS SECOND TRY

--- decorators.py
+++ decorators.py
@@ -1,31 +1,26 @@
 '''
 ## Create a decorator that decorates a function with one or more arguments.
 - This decorator should check that the argument is of type str and the length of the str is more than 5 
-- else raise an exception of type ValueError telling the user the arguments should be str and it should have more than 5 characters .
+- else raise an exception of type ValueError telling the user the arguments should be str and .
 '''
 
 
 
 def document_it(func:str):
-    def wrapper(*args, **kwargs):
-    
-        if len(text)>=6  :
+    def wrapper(*args):
+
+        if len(text)>=6  and isinstance(text,str) :
             print("you input is string and have more than 5 character")
-            return func(*args, **kwargs)
-        else :
-            print("you input may not string or have less than 5 character")
-            return func(*args, **kwargs)
-        
+            return func(*args)
+        elif len(text) <6 or not isinstance(text,str):
+            raise ValueError("ERROR:you input may not string and it should have more than 5 characters")
+         
     return wrapper
 
 
 @document_it
 def check(text:str):
     print("checked !")
-
-text:str=input("please enter your text:\n")
-try:
-    check(text)
-except ValueError:
-    print("VALUE ERROR: the arguments should be str and it should have more than 5 characters")    
-
+      
+text=input("please enter your text:\n")
+check(text)
