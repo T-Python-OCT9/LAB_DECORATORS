f12623b98e97b1dee9666768b81c9a9da280661b
moath ghazi alosaimi <104032201+Oneshotqq@users.noreply.github.com>
2022-10-19 16:11:33 +0300

    updated

--- Lab_decorators.py
+++ Lab_decorators.py
@@ -3,16 +3,17 @@ from ast import arg
 
 def document_it(argument : str):
     def wrapper (*args , **kwargs):
-        if len(text) >= 5 and type(text) == str :
+        if len(text) > 5 and type(text) == str :
             print ("what u typed is string and more then 5 ")
             return argument (*args,**kwargs)
-        else:  not len(text) >= 5 and not type(text) == str 
-        return argument(*args,**kwargs)
+        else:  
+            raise ValueError("arguments should be str and it should have more than 5 characters") #not len(text) >= 5 and not type(text) == str 
+        #return argument(*args,**kwargs)
     return wrapper
 
 @document_it
 def  check_string_type (text: str) :
-    print ("is less then 5 and type is not string")
+    print ()
 
 text = str(input(" please enter your text : "))
 try :
