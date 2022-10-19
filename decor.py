195b8f32372f74959c3c2eb77cc4dbe6a91b25e7
Nourah Alqhtani <nourah0qq@gmail.com>
2022-10-19 17:09:58 +0300

    Final Lab documenting the function

--- decor.py
+++ decor.py
@@ -1,24 +1,32 @@
+
 def document_it(func):
 
     def wrapper(*args, **kwargs):
         print("Documenting the function, name : ", func.__name__)
-        print(len(*args))
-        if type(*args, **kwargs) == str:
-            if len(args) > 5:
-                print("Documenting the function, name : ", func.__name__)
+        for arg in args:
+            if not (isinstance(arg,str)and len(arg)>5):
+                raise ValueError("value should be string and it should have more than 5 characters")
+        for kw in kwargs:
+            if not (isinstance(kw,str)and len(kw)>5):
+                raise ValueError("value should be string and it should have more than 5 characters")
+
+        
         return func(*args, **kwargs)
     
     return wrapper
 
 @document_it
-def greet(strr,strr2):
-    i = len(strr)
+def func_str(strr,strr2):
     print("Welcome ! ", strr,strr2)
 
-name = "nourah"
-name2 = "hyaa"
-greet(name,name2)
-i = 7
+
+
+name1 = "Nourah"
+name2 = "Alqahtani"
+try:
+    func_str(name1,name2)
+except ValueError as e:
+    print(e)
 
 
 
